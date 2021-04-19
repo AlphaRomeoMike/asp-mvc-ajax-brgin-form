e64d64f415cf4f70d820b64ca95b1f0b8f7c8f5c
Abdul Rafay Modi <subjectalpha32@gmail.com>
2021-04-19 13:26:25 +0500

    Add project files.

---
+++ Ajax Forms/Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using Ajax_Forms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ajax_Forms.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeEntities db = new EmployeeEntities();
+        // GET: Employee
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(employee e)
+        {
+            if(ModelState.IsValid == true)
+            {
+                db.employees.Add(e);
+                int em = db.SaveChanges();
+                if(em > 0)
+                {
+                    return Json("Inserted", JsonRequestBehavior.AllowGet);
+                } 
+                else
+                {
+                    return Json("Not inserted");
+                }
+            }
+            return View();
+        }
+    }
+}
